e72bb105676a6a6d661f16b0f88cfdf9d7281e40
maximilien <maximilien01993@gmail.com>
2020-08-17 17:37:48 +0200

    Fin Exercice Article

--- C#/Orienté Objet/Article/Article/Program.cs
+++ C#/Orienté Objet/Article/Article/Program.cs
@@ -10,6 +10,33 @@ namespace Article
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.Write("Donner le taux de TVA pour tous les articles : ");
+
+            Article.TauxTVA = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Le taux pour tous les articles est de : {Article.TauxTVA}%");
+
+            Article article1 = new Article();
+            Article article2 = new Article("1","Chaise en bois");
+            Article article3 = new Article("2", "Meuble de salon", 10.0);
+            Article article4 = new Article(article2);
+
+            article1.AfficherArticle();
+            Console.WriteLine();
+
+            article2.AfficherArticle();
+            Console.WriteLine();
+
+            article3.AfficherArticle();
+            Console.WriteLine();
+
+            article4.AfficherArticle();
+            Console.WriteLine();
+
+            Console.ReadLine();
+
         }
     }
 }
